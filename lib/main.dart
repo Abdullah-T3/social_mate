f069f4625a58f2b75aefdabd358bea6f6c0ea80d
Abdullah-T3 <eng.abdullahahmed59@gmail.com>
2025-05-03 05:59:54 +0300

    //

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'core/routing/appRouting.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  await dotenv.load();
   await initDependencies();
 
   // Run the app with DevicePreview enabled
